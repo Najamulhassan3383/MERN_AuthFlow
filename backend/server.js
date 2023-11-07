4547c535770d89ff530ce7c0ede3355522e9659d
Najamulhassan3383 <najmulhassan721@gmail.com>
2023-11-07 08:56:43 +0500

    added the protect middlware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import UserRouter from "./routes/Userroutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 connectDB();
 
 app.use("/api/users", UserRouter);
