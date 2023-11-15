e469f21f3103a545b5d19ab4216a2dc4c4f97ac1
Najamulhassan3383 <najmulhassan721@gmail.com>
2023-11-15 21:44:25 +0500

    added route protection

--- mern_authflow/src/components/ProfilePage.jsx
+++ mern_authflow/src/components/ProfilePage.jsx
@@ -1,11 +1,18 @@
-import  { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const ProfilePage = () => {
-  // Assuming you have a user object with properties like name, email, and picture
-
-  // State for handling form input
+  const { user } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/");
+    }
+  }, [user, navigate]);
 
   // Function to handle profile update
   const handleUpdateProfile = () => {
@@ -15,51 +22,57 @@ const ProfilePage = () => {
 
   return (
     <div className="max-w-md mx-auto mt-8 p-6 bg-white rounded-md shadow-md">
-      <img
-        className="w-20 h-20 mx-auto mb-4 rounded-full object-cover"
-        src="./najam.jpg" // Replace with the actual URL of the user's profile picture
-        alt="Profile"
-      />
-      <div className="mb-4">
-        <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlFor="password"
-        >
-          Email
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="password"
-          type="password"
-          placeholder="Enter your new email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
+      {user ? (
+        <>
+          <img
+            className="w-20 h-20 mx-auto mb-4 rounded-full object-cover"
+            src={user.profilePicture} // Replace with the actual URL of the user's profile picture
+            alt="Profile"
+          />
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="text"
+              placeholder="Enter your new email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
 
-      <div className="mb-4">
-        <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlFor="password"
-        >
-          Password
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="password"
-          type="password"
-          placeholder="Enter your new password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Enter your new password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
 
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        onClick={handleUpdateProfile}
-      >
-        Update Profile
-      </button>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            onClick={handleUpdateProfile}
+          >
+            Update Profile
+          </button>
+        </>
+      ) : (
+        <h1>Sign in Plz</h1>
+      )}
     </div>
   );
 };
